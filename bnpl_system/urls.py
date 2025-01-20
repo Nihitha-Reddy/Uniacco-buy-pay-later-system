5bf0bde8507873566b7a5000f4280bb228c16464
Nihitha-Reddy <nihithareddy009@gmail.com>
2025-01-20 12:10:01 +0530

    Delete bnpl_system directory

--- bnpl_system/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns = [
-   path('register/', views.RegisterUser.as_view(), name='register'),
-   path('purchase/', views.RecordPurchase.as_view(), name='record_purchase'),
-   path('repayment-plan/', views.CreateRepaymentPlan.as_view(), name='create_repayment_plan'),
-   path('payment/', views.RecordPayment.as_view(), name='record_payment'),
-]
