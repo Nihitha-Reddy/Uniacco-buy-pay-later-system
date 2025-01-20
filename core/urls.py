39589983522aa5ceba24af178216c9bbccaf8297
Nihitha-Reddy <nihithareddy009@gmail.com>
2025-01-20 12:53:52 +0530

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+
 urlpatterns = [
-   path('register/', views.RegisterUser.as_view(), name='register'),
-   path('purchase/', views.RecordPurchase.as_view(), name='record_purchase'),
-   path('repayment-plan/', views.CreateRepaymentPlan.as_view(), name='create_repayment_plan'),
-   path('payment/', views.RecordPayment.as_view(), name='record_payment'),
+    path('register/', views.register_user, name='register_user'),
+    path('purchase/', views.record_purchase, name='record_purchase'),
+    path('payment/', views.record_payment, name='record_payment'),
+    path('repayment_plans/<str:user_id>/', views.get_repayment_plans, name='get_repayment_plans'),
+    path('emi_balance/<str:user_id>/', views.get_emi_balance, name='get_emi_balance'),
 ]
